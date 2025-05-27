eea8c59bcfef4d6bb9ec8efa50a803df3cf24c35
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-27 10:04:39 +0600

    Nested Dynamic Routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default async function ProductReview({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+  return (
+    <div>
+      Review {reviewId} for product {productId}
+    </div>
+  );
+}
