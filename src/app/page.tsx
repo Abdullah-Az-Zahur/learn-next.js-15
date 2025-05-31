a1745eec7c122389f47a813e80896486026e592c
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-31 20:28:17 +0600

    upgrate tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>Welcome Home</h1>
+      <h1 className="text-red-500">Welcome Home</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/products">Products</Link>
     </>
