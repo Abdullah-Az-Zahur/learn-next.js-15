098029f88e0cdebfdaea27861d318442a11ba9fb
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-09-21 23:51:45 +0600

    Headers in Route Handlers

---
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -0,0 +1,16 @@
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  //    const requestHeaders = new Headers(request.headers);
+  //    console.log(requestHeaders.get("Authorization"))
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("Portfolio API data", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+}
