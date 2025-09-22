6e9665c9c04b2c35aa3293baff21a609a17a8dd4
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-09-22 12:40:52 +0600

    Cookies in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -8,9 +8,17 @@ export async function GET(request: NextRequest) {
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
 
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "20");
+  console.log(cookieStore.get("resultPerPage"));
+
   return new Response("Portfolio API data", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
